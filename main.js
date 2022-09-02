2711dccd449a5cee74eefb0df9698b9c96c439b7
charlie-zzy <neycbs@outlook.com>
2022-09-02 12:25:58 +0800

    添加缓存机制

--- main.js
+++ main.js
@@ -26,112 +26,159 @@ let Dialog = null
 const DIALOG_TEXT = `<div class="mdui-dialog" id="dialog"><div class="mdui-dialog-title">自定义音乐</div><div class="mdui-dialog-content"><div class="mdui-textfield"><label class="mdui-textfield-label">名称</label><input class="mdui-textfield-input" id="dialog-name" type="text" required /><div class="mdui-textfield-error">名称不能为空，不然你咋区分</div></div><div class="mdui-textfield"><label class="mdui-textfield-label">单曲ID或分享链接</label><input class="mdui-textfield-input" id="dialog-url" type="text" /><div class="mdui-textfield-error" id="dialog-url-error">无法解析</div><div class="mdui-textfield-helper">在网易云打开单曲，复制分享链接然后粘贴到这里</div></div></div><div class="mdui-dialog-actions"><button class="mdui-btn mdui-ripple" mdui-dialog-cancel>取消</button><button class="mdui-btn mdui-ripple" id="dialog-save" mdui-dialog-confirm>保存</button></div></div>`
 let editingIndex = null
 
-function refreshSongPreset() {
-  $('.audio-card').empty()
-  songPreset.forEach(({ title, icon, text }, id) => {
-    if (id % 3 == 0) $('.audio-card').append(`<div id="audio-col-${id / 3}">`)
-    const item = $(`
+songPreset.forEach(({ title, icon, text }, id) => {
+  if (id % 3 == 0) $('.audio-card').append(`<div id="audio-col-${id / 3}">`)
+  const item = $(`
     <div class="audio-card-item" id="item-${id}">
     <div class="title">${title}</div>
-    ${text ? `<i class="mdui-icon material-icons">${icon}</i>`
-        : `<div class="half-circle-spinner">
-    <div class="circle circle-1"></div>
-    <div class="circle circle-2"></div>
-    </div>`}
-    <div class="filename">${text ?? '加载中~'}</div>
+    <i class="mdui-icon material-icons">file_download</i>
+    <div class="filename">点击加载</div>
     </div>`)
-    item.on('click', () => {
-      if (!songBuf[id]) return
-      if (!mode) {
-        if (BufferNode && BufferNode.state == 'playing') {
-          BufferNode.onended = null
-          BufferNode.stop()
-          BufferNode.state = 'closed'
+  item.on('click', () => {
+    if (!songBuf[id]) {
+      loadBuffer(songPreset[id], id)
+      return
+    }
+    if (!mode) {
+      if (BufferNode && BufferNode.state == 'playing') {
+        BufferNode.onended = null
+        BufferNode.stop()
+        BufferNode.state = 'closed'
+      }
+      BufferNode = null
+      BufferNode = audioCtx.createBufferSource()
+      BufferNode.buffer = songBuf[id]
+      BufferNode.connect(GainNode)
+      BufferNode.state = 'playing'
+      BufferNode.startTime = Date.now()
+      BufferNode.start(0)
+      $(`#status`).text('PLAY')
+      $(`#countdown`).addClass('mdui-color-red')
+      $(`#countdown`).removeClass('mdui-color-green')
+      BufferNode.onended = () => {
+        $(`#status`).text('NEXT')
+        $(`#countdown`).removeClass('mdui-color-red')
+        $(`#countdown`).addClass('mdui-color-green')
+      }
+    }
+  })
+  item.on('contextmenu', (evt) => {
+    evt.preventDefault()
+    Dialog = new mdui.Dialog(DIALOG_TEXT, {
+      closeOnConfirm: false, destroyOnClosed: true, modal: true
+    })
+    const $name = $('#dialog-name'), $url = $('#dialog-url'), $error = $('#dialog-url-error')
+    $name.on('keydown', ({ key }) => {
+      if (key == 'Enter') $('#dialog-save')[0].click()
+    })
+    $url.on('keydown', ({ key }) => {
+      if (key == 'Enter') $('#dialog-save')[0].click()
+    })
+    $('#dialog-save').on('click', async () => {
+      const title = $name.val(), url = $url.val()
+      let result = null
+      if (url.match(/(?<=song\??id=)\d+/) != null) //  匹配成功
+        result = url.match(/(?<=song\??id=)\d+/)[0]
+      else if (url.match(/[^\d]/) != null) // 含有未知字符
+        $error.text('是无法理解的内容呢qwq')
+      else if (url != "") // 纯数字，推测为 ID
+        result = parseInt(url)
+      else // 根本没输
+        $error.text('请输入URL...')
+
+      if (!result) {
+        $error.parent().addClass('mdui-textfield-invalid')
+      } else {
+        $error.parent().removeClass('mdui-textfield-invalid')
+        $('#dialog-save').text('识别中')
+        $('.mdui-dialog-actions button').attr('disabled', true)
+        const res = await fetch('https://api.i-meto.com/meting/api?type=song&id=' + result).then(data => data.json())
+        if (res.length == 0) {
+          $('#dialog-save').text('识别失败了qwq')
+          setTimeout(() => {
+            $('.mdui-dialog-actions button').removeAttr('disabled')
+            $('#dialog-save').text('保存')
+          }, 1000)
+          return
         }
-        BufferNode = audioCtx.createBufferSource()
-        BufferNode.buffer = songBuf[id]
-        BufferNode.connect(GainNode)
-        BufferNode.state = 'playing'
-        BufferNode.startTime = Date.now()
-        BufferNode.start(0)
-        $(`#status`).text('PLAY')
-        $(`#countdown`).addClass('mdui-color-red')
-        $(`#countdown`).removeClass('mdui-color-green')
-        BufferNode.onended = () => {
-          $(`#status`).text('NEXT')
-          $(`#countdown`).removeClass('mdui-color-red')
-          $(`#countdown`).addClass('mdui-color-green')
-        }
+        const { url } = res[0]
+        songPreset[editingIndex].url = url
+        songPreset[editingIndex].title = title
+        data.set('songPreset', songPreset)
+        await localforage.removeItem('cache-' + id)
+        loadBuffer(songPreset[editingIndex], editingIndex)
+        $('#dialog-save').text('解析成功喵~')
+        setTimeout(() => {
+          Dialog.close()
+        }, 600)
       }
     })
-    item.on('contextmenu', (evt) => {
-      evt.preventDefault()
-      Dialog = new mdui.Dialog(DIALOG_TEXT, {
-        closeOnConfirm: false, destroyOnClosed: true, modal: true
-      })
-      const $name = $('#dialog-name'), $url = $('#dialog-url'), $error = $('#dialog-url-error')
-      $('#dialog-save').on('click', async () => {
-        const title = $name.val(), url = $url.val()
-        let result = null
-        console.log(url.match(/\D*/), url);
-        if (url.match(/(?<=song\??id=)\d+/) != null) //  匹配成功
-          result = url.match(/(?<=song\??id=)\d+/)[0]
-        else if (url.match(/[^\d]/) != null) // 含有未知字符
-          $error.text('是无法理解的内容呢qwq')
-        else if (url != "") // 纯数字，推测为 ID
-          result = parseInt(url)
-        else // 根本没输
-          $error.text('请输入URL...')
-
-        if (!result) {
-          $error.parent().addClass('mdui-textfield-invalid')
-        } else {
-          $error.parent().removeClass('mdui-textfield-invalid')
-          $('#dialog-save').text('解析链接中')
-          $('.mdui-dialog-actions button').attr('disabled', true)
-          const res = await fetch('https://api.i-meto.com/meting/api?type=song&id=' + result).then(data => data.json())
-          if (res.length == 0) {
-            $('#dialog-save').text('解析失败了...')
-            setTimeout(() => {
-              $('.mdui-dialog-actions button').removeAttr('disabled')
-              $('#dialog-save').text('保存')
-            }, 1000)
-            return
-          }
-          const { url } = res[0]
-          songPreset[editingIndex].url = url
-          songPreset[editingIndex].title = title
-          data.set('songPreset', songPreset)
-          loadBuffer(songPreset[editingIndex], editingIndex)
-          $('#dialog-save').text('解析成功喵~')
-          setTimeout(() => {
-            Dialog.close()
-          }, 1000)
-        }
-      })
-      Dialog.open()
-      $name.val(songPreset[id].title)
-      if (songPreset[id].url.match(/id=\d+/))
-        $url.val(songPreset[id].url.match(/id=\d+/)[0].split('=')[1])
-      editingIndex = id
-      $('#dialog').mutation()
-      Dialog.handleUpdate()
-    })
-    $(`#audio-col-${~~(id / 3)}`).append(item)
+    Dialog.open()
+    $name.val(songPreset[id].title)
+    if (songPreset[id].url.match(/id=\d+/))
+      $url.val(songPreset[id].url.match(/id=\d+/)[0].split('=')[1])
+    editingIndex = id
+    $('#dialog').mutation()
+    Dialog.handleUpdate()
   })
-}
-refreshSongPreset()
+  $(`#audio-col-${~~(id / 3)}`).append(item)
+})
 
+async function fetchWithProcess(url, fn) {
+  const { body: data, headers } = await fetch(url)
+  const Totallength = +headers.get('Content-Length'), reader = data.getReader()
+
+  let receivedLength = 0, result = new Uint8Array(Totallength)
+  while (true) {
+    const { done, value } = await reader.read()
+    if (done) break
+    result.set(value, receivedLength)
+    receivedLength += value.length
+    fn(receivedLength / Totallength)
+  }
+
+  return result.buffer
+}
 async function loadBuffer({ url }, id) {
-  await fetch(url)
-    .then((res) => res.arrayBuffer())
-    .then((buf) => audioCtx.decodeAudioData(buf))
-    .then((buf) => {
-      console.log('%cloaded', 'color:lightgreen;border: 1px lightgreen solid;border-radius:4px;padding:0 4px', id, url)
-      songBuf[id] = buf
-      songPreset[id].text = '✓ ' + TtoMMSS(buf.duration)
-      refreshSongPreset()
+  const el = $($('#item-' + id).children()[2])
+  console.log('%cstart loading', 'color:yellow;border: 1px yellow solid;border-radius:4px;padding:0 4px', id)
+  $('#item-' + id).children()[1].innerHTML = `<div class="half-circle-spinner"><div class="circle circle-1"></div><div class="circle circle-2"></div></div>`
+  songPreset[id].loading = true
+
+  el.text('加载缓存...')
+  const cache = await localforage.getItem('cache-' + id)
+  if (!cache) {
+    el.text('未找到缓存')
+    await fetchWithProcess(url, (precent) => {
+      el.text('下载中 ' + (precent * 100).toFixed() + '%')
     })
+      .then(async (buf) => {
+        el.text('缓存中...')
+        await localforage.setItem('cache-' + id, buf.slice())
+        el.text('解码中...')
+        return audioCtx.decodeAudioData(buf)
+      })
+      .then((buf) => {
+        console.log('%cloaded', 'color:lightgreen;border: 1px lightgreen solid;border-radius:4px;padding:0 4px', id, url)
+        songBuf[id] = null
+        songBuf[id] = buf
+        $($('#item-' + id).children()[1]).text(songPreset[id].icon)
+        el.text('✓ 成功 ')
+        setTimeout(() => el.text('✓ ' + TtoMMSS(buf.duration)), 800)
+      })
+  }
+  else {
+    el.text('解析中...')
+    await audioCtx.decodeAudioData(cache)
+      .then((buf) => {
+        console.log('%cloaded', 'color:lightgreen;border: 1px lightgreen solid;border-radius:4px;padding:0 4px', id, url)
+        songBuf[id] = null
+        songBuf[id] = buf
+        $($('#item-' + id).children()[1]).text(songPreset[id].icon)
+        el.text('✓ 成功 ')
+        setTimeout(() => el.text('✓ ' + TtoMMSS(buf.duration)), 800)
+      })
+  }
 }
 songPreset.forEach(loadBuffer)
 
@@ -166,6 +213,7 @@ timer.setMutation((id, d) => {
     BufferNode.stop()
     BufferNode.state = 'closed'
   }
+  BufferNode = null
   BufferNode = audioCtx.createBufferSource()
   BufferNode.buffer = songBuf[id]
   BufferNode.connect(GainNode)
@@ -206,7 +254,8 @@ function handleDel(el) {
   el.parentNode.parentNode.remove()
 }
 
-function handleResetSong() {
+async function handleResetSong() {
   data.del('songPreset')
+  await localforage.dropInstance()
   location.reload()
 }
